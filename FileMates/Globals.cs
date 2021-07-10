2b552cf1812d574797345233fc6fc33eaf78c6d0
VulpesPaw <richard.jh.johansson@gmail.com>
2021-07-10 19:34:04 +0200

    Initial Uppload

---
+++ FileMates/Globals.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace FileMates
+{
+    internal class Globals: FileObject
+    {
+        // paths
+        public string original, search, destination;
+
+        public List<string> filenamesOriginal = new List<string>();
+        public List<string> filenamesSearch = new List<string>();
+        public List<string> fullpathsSearch = new List<string>();
+        public List<string> fileMatch = new List<string>();
+
+        // Add filename as key and all paths in list
+        //? -- old
+        //public IDictionary<string, FileObject> fileDictionary = new Dictionary<string, FileObject>(); 
+        
+        public IDictionary<string, List<string>> fileDictionary = new Dictionary<string, List<string>>();
+    }
+}
