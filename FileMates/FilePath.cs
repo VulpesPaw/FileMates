7b92f36d96cc23f41aa5f3843f4b888ac91ff31e
VulpesPaw <richard.jh.johansson@gmail.com>
2021-10-19 19:52:54 +0200

    Initial Creation

---
+++ FileMates/FilePath.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Data;
+using System.Collections.Generic;
+using System.Linq;
+using System.IO;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FileMates
+{
+    internal class FilePath
+
+    {
+        // full file path from root
+        private string @_fullFilePath;
+
+        // full filename including exctentions
+        private string @_filenameWithExtention;
+
+        public FilePath(string fullFilePath, string filenameWithourExtention)
+        {
+            this._fullFilePath = fullFilePath;
+            this._filenameWithExtention = filenameWithourExtention;
+        }
+
+        public string FullFilePath { get { return _fullFilePath; } }
+        public string FilenameWithExtention { get { return _filenameWithExtention; } }
+    }
+}
